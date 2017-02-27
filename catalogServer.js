96d0369e4220b3f0867e1d39cdfc18a1dedfedbb
EthanHaley <ethan.haley93@gmail.com>
2017-02-27 17:30:37 -0600

    More skeleton work

--- catalogServer.js
+++ catalogServer.js
@@ -25,7 +25,7 @@ function handleRequest(req, res) {
 				uploadForm(req, res);
 			}
 			else if(req.method == 'POST') {
-				uploadItem(req, res);
+				uploadEntry(req, res);
 			}
 			break;
 		//Favicon
@@ -46,4 +46,72 @@ function handleRequest(req, res) {
 	}
 }
 
+function buildCatalog(imageTage) {
+	return template.render('catalog', {
+		title: config.title,
+		imageTags: imageNamesToTags(imageTags),join('')
+	});
+}
 
+function serveCatalog(req, res) {
+	getImageNames(function(err, imageNames) {
+		if(err) {
+			console.error(err);
+			res.statusCode = 500;
+			res.statusMessage = "Server Error";
+			res.end();
+			return;
+		}
+		res.setHeader('COntent-Type', 'text/html');
+		res.end(buildCatalog(imageNames));
+	});
+}
+
+function getImageNames(callback) {
+	fs.readdir('images/', function(err, fileNames) {
+		if(err) callback(err, undefined);
+		else callback(false, fileNames);
+	});
+}
+
+function imageNamesToTags(fileNames) {
+	return fileNames.map(function(fileName) {
+		return '<img src="${fileName}" alt="${fileName}">';
+	});
+}
+
+function serveImage(fileName, req, res) {
+	fs.readFile('images/' + decodeURIComponent(fileName), function(err, data) {
+		if(err) {
+			console.error(err);
+			res.statusCode = 500;
+			res.statusMessage = "Resource not Found";
+			res.end()
+			return;
+		}
+		res.setHeader("Content-Type", "image/jpeg");
+		res.end(data);
+	});
+}
+
+function uploadImage(req, res) {
+	multipart(req, res, function(req, res) {
+		if(!req.body.image.filename) {
+			console.error("No file in upload");
+			res.statusCode = 400;
+			res.statusMessage = "No file specified";
+			res.end("No file specified");
+			return;
+		}
+		fs.writeFile('images/' + req.body.image.filename, req.body.image.data, function(err) {
+			if(err) {
+				console.error(err);
+				res.statusCode = 500;
+				res.statusMessage = "Server Error";
+				res.end("Server Error");
+				return;
+			}
+			serveCatalog(req, res);
+		});
+	});
+}
